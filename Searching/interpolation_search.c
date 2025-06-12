5f91470ba1af833d3aaefbb90b2e10f643eba84e
optanay <aronferg007@gmail.com>
2025-06-12 22:23:29 +0530

    Update interpolation_search.c

--- Searching/interpolation_search.c
+++ Searching/interpolation_search.c
@@ -7,13 +7,13 @@ int interpolationSearch(int arr[], int n, int key) {
         int pos = low + ((key - arr[low]) * (high - low)) / (arr[high] - arr[low]);
 
         if (arr[pos] == key)
-            return pos;       // found
+            return pos;       
         else if (arr[pos] < key)
-            low = pos + 1;    // search right subarray
+            low = pos + 1;   
         else
-            high = pos - 1;   // search left subarray
+            high = pos - 1;   
     }
-    return -1;  // not found
+    return -1;  
 }
 
 int main() {
